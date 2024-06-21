d935b05aaaa19905276974e738dd8f075a79a6e7
Sawdo99 <155194856+Sawdo99@users.noreply.github.com>
2024-06-21 12:45:26 -0500

    First Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+﻿from stories import Story
+
+@app.route("/")
+def ask_questions():
+    """asks question."""
+
+    prompts = story.prompts
+
+    return render_template("page.html", prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    """Show story."""
+
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
